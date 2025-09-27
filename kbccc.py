9fa76a386b32077717883991755833d487d70b60
Fasil Ashraf <itsfaisal0192@gmail.com>
2025-09-27 16:12:20 +0530

    add some content

--- kbccc.py
+++ kbccc.py
@@ -15,15 +15,16 @@ questions = [["which building is the tallest building in the world?","Burj khali
              ["which building is the tallest building in the world?","Burj khalifa","taj mahal","qutub minar","red fort",1],
              ]
 levels =[1000,2000,3000,4000,5000,10000,20000,40000,80000,160000,320000,640000,]
+money = 0 
 for i in range (0,len(questions)):
     question = questions[i]
-    print(f"\n\n[Question for rupees.{levels[i]}")
-    print(f"a. {question[1]}       b. {question[2]}")
-    print(f"c. {question[2]}       d. {question[4]}")
-    reply =int(input("Enter your answer in (1-4) form: "))
-    money = 0  
+    print(f"\nQuestion for rupees.{levels[i]}")
+    print(f"1. {question[1]}       2. {question[2]}")
+    print(f"3. {question[2]}          4. {question[4]}")
+    reply =int(input("Enter your answer : "))
+     
     if reply == question[-1]:
-        print (f"correct answer, You have won Rs {levels[i]}") 
+        print (f"\ncorrect answer, You have won Rs {levels[i]}") 
         if (i ==4):
             money = 10000
         elif(i ==9):
@@ -31,6 +32,6 @@ for i in range (0,len(questions)):
         elif(i ==14):
             money =10000000    
     else:
-        print("Wrong Answer")
-        break    
-    
+        print("Wrong Answer .")
+        break 
+print(f"your take home money is ${money}")   
